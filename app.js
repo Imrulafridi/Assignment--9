3a2c499232698b689daca4a870eac5ce387d6346
Imranullah Afridi <imrulafridi@gmail.com>
2020-07-16 00:25:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,187 @@
+// ====== Chapter From 38 To 42 ======
+
+// ====== Task # 1
+// function power(){
+//   const a = prompt("Enter Your Desire Number")
+//   const b = a*a;
+//   alert("Power Of Your Number is : " + b)
+
+// }
+// power()
+
+
+// ====== Task # 2
+// function leapYear(){
+//   const a = prompt("Enter Any Year To See Its Leap Year Or Not")
+//   const b = a%4
+//   if(b === 0){
+//     alert("Its A Leap Year")
+//   }else{
+//     alert("Its Not a Leap Year")
+//   }
+// }
+// leapYear()
+
+
+// ====== Task # 3
+// function triangleArea(){
+//   const a = prompt("Enter Fisrt Side")
+//   const b = prompt("Enter Second Side")
+//   const c = prompt("Enter Third Side")
+//   const s = (a+b+c)/2;
+//   const area = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+//   const x = Math.floor(area)
+//   alert("Area Of The Triangle is : " + x)
+// }
+// triangleArea()
+
+
+// ====== Task # 4
+// function main(){
+//   var s1 = prompt("Enter Your Marks in subjet 1")
+//   var s2 = prompt("Enter Your Marks in subjet 2")
+//   var s3 = prompt("Enter Your Marks in subjet 3")
+
+//   function average(){
+//     var avg = Number((s1+s2+s3)/3)
+//     alert("Your Average Marks are : " + avg)
+//     return avg;
+//   }
+//   function percentage(){
+//     var percnt = ((s1+s2+s3)/300)*100
+//     alert("Your Percentage is : " + percnt)
+//     return percnt;
+//   }
+
+// }
+
+// main()
+
+
+
+// ====== Task # 5
+
+// ====== Task # 6
+// function removeVowel(){
+//   const str = prompt("Write Something To remove Vowel From It")
+//   const newse = str.length
+//   switch(newse){
+//     case 'a' :
+//     str.replace()
+//     break
+//     case 'e' :
+//     str.replace()
+//     break
+//     case 'i' :
+//     str.replace()
+//     break
+//     case 'o' :
+//     str.replace()
+//     break
+//     case 'u' :
+//     str.replace()
+//     break
+//     default :
+//     alert("There is No Vowel")
+//   }
+//   alert("Your New Sentence Is : " + str)
+// }
+// removeVowel()
+
+
+// ====== Task # 7
+
+// ====== Task # 8
+// const dist = prompt("Enter The Distance Bt Two Cities")
+
+// function meter(){
+//   const meter = dist*1000
+//   alert ("The Distance is " + meter + " meter")
+// }
+// meter()
+
+// function feet(){
+//   const feet = dist*3280.84
+//   alert ("The Distance is " + feet + " feet")
+// }
+// feet()
+
+// function inches(){
+//   const inches = dist*39370.1
+//   alert ("The Distance is " + inches + " inches")
+// }
+// inches()
+
+// function centi(){
+//   const centi = dist*1000*100
+//   alert ("The Distance is " + centi + " centimeter")
+// }
+// centi()
+
+
+// ====== Task # 9
+// let work = prompt("Enter The Amount Of Hour You Work For This Week")
+// let w;
+// switch(w){
+//   case work > 40 :
+//     w = work*12;
+//     alert(w)
+//     break;
+//   default :
+//   alert("No Overtime")
+// }
+
+
+// ====== Task # 10
+
+
+
+// ====== Chapter From 43 To 48 ======
+
+// ====== Task # 3
+// function invisible(){
+//   document.getElementById("remove").className = "hidden"
+// }
+
+
+// ====== Task # 4
+// function before(firstimg) {
+//   firstimg.src = "v10.jpg";
+// }
+
+// function after(secondimg) {
+//   secondimg.src = 'products-Samsung_Galaxy_S6_Edge_Plus_Gold_main.jpg';
+// }
+  
+
+// ====== Task # 5
+// var num = 0
+// function increase(){
+//   var number = document.getElementById("number")
+//   number.value = ++num
+// }
+// function decrease(){
+//   var number = document.getElementById("number")
+//   number.value = --num
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====== Task # 1
+// ====== Task # 1
+
+
+
